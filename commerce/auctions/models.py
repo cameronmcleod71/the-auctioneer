86fa7584fe02c2c1cdd9473b24d4ea13db823ae0
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-01-11 20:23:40 -0600

    added some basic models that the project will need. Models: Listing, Bid, Comment, Watchlist, Category

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    current_price = models.DecimalField(decimal_places=2, max_digits=12)
+    description = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, related_name="listings")
+
+
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+class Watchlist(models.Model):
+    pass
+
+class Category:
+    type = models.CharField(max_length=64)
+
+# maybe one more model for categories -- categories, each with their own id
